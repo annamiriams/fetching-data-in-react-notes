d02436a7c39cc8b06aa328aea25107bb618542d4
annams <annaextrasteckel@gmail.com>
2025-03-20 17:11:06 -0700

    updated UI to reflect searched weather data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 // This syntax imports everything (*) exported from the weatherService.js module and groups them inside a weatherService object. Whenever we require a specific service function, we can access it through dot notation on the weatherService object.
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch.jsx'
+import WeatherDetails from './components/WeatherDetails/WeatherDetails.jsx'
 import { useState } from 'react';
 
 // Here, we call upon fetchData() with an onClick event handler and log the data returned from weatherService.show('New York') to the console.
@@ -33,6 +34,7 @@ const App = () => {
             <h1>Weather API</h1>
             {/* <button onClick={fetchData}>Fetch Weather Data</button> */}
             <WeatherSearch fetchData={fetchData}/>
+            <WeatherDetails weather={weather}/>
         </main>
     );
 };
