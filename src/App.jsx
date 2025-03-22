1f2cccb54380bb51c025724f773f72b73f900d0c
annams <annaextrasteckel@gmail.com>
2025-03-21 21:25:17 -0700

    background data fetching lesson complete; useEffect() hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.css';
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch.jsx'
 import WeatherDetails from './components/WeatherDetails/WeatherDetails.jsx'
-import { useState } from 'react';
+// adding  in useEffect
+import { useState, useEffect } from 'react';
 
 // Here, we call upon fetchData() with an onClick event handler and log the data returned from weatherService.show('New York') to the console.
 const App = () => {
@@ -28,6 +29,25 @@ const App = () => {
     };
 
     console.log('State:', weather);
+
+    // adding in useEffect hook so that default (New York) data is rendered on the page when it's initially loaded
+    useEffect(() => {
+        // define a fetch function:
+        const fetchDefaultData = async () => {
+            const data = await weatherService.show('New York');
+            const newWeatherState = {
+                location: data.location.name,
+                temperature: data.current.temp_f,
+                condition: data.current.condition.text,
+            };
+            setWeather(newWeatherState);
+        };
+            
+            // without calling this function, we've only defined the fetchDefaultData function and as a result, we won't see anything in the UI. without the next line of code, every time the useEffect() hook runs, the above only recreates the fetchDefaultData() function.
+            fetchDefaultData();
+
+    // an empty dependency array (below) means that this runs once after the initial rerender 
+    }, []);
 
     return (
         <main>
